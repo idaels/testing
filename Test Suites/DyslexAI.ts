36b0c9e64079ca3ef7c9954d25b45eafb4262494
Ilse <ilse.daels@ugent.be>
2023-03-16 13:06:22 +0100

    adding collection

--- Test Suites/DyslexAI.ts
+++ Test Suites/DyslexAI.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0bbeb663-7d54-4904-b776-95b5963e6896</testSuiteGuid>
+   <testCaseLink>
+      <guid>0a562c3a-6d7d-47a7-81d8-972b7847418b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DyslexAI-summarizer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
