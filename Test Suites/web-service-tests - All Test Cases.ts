43cce95941f9aafdac76abd4fc82fc36171ca407
Ilse <ilse.daels@ugent.be>
2023-04-02 17:00:06 +0200

    added simplifier

--- Test Suites/web-service-tests - All Test Cases.ts
+++ Test Suites/web-service-tests - All Test Cases.ts
@@ -12,27 +12,6 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>df88b384-4db8-4c82-b255-0afa0fd22006</testSuiteGuid>
    <testCaseLink>
-      <guid>34dc3e3d-2ddf-4029-b1d7-213081330ba6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/DyslexAI-summarizer</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a0de065a-6716-4ab9-be3b-8ec7c73f66e4</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/DyslexAi-semantic-search</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>39aab808-faa9-4fd4-a2d5-2d601d87d0b2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/DyslexAi-normalizer</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>4edb06ba-9ab0-49b8-9655-230138ba406b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -51,6 +30,13 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/DyslexAi-normalizer-nondev</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69ef3494-4e72-4050-a488-92a7b9adc6f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DyslexAi-simplifier-nondev</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
